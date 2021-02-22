a871f843f7b29c5e78da102b9b1748fc47be0f8d
silverawz <deroussenicolas@gmail.com>
2021-02-22 21:18:10 +0100

    animation done for all pieces except start and last, game is working for now

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-/*
-var board = [[null, null, null, null],[null, null, null, null],[null, null, null, null],[null, null, null, null],
-            [null, null, null, null],[null, null, null, null],[null, null, null, null],[null, null, null, null],
-            [null, null, null, null],[null, null, null, null],[null, null, null, null],[null, null, null, null],
-            [null, null, null, null],[null, null, null, null],[null, null, null, null],[null, null, null, null]];
-            */
+
            var board = [];
            var squares = [];
            var nbreCase = 64;
-           var currentPiece = null;
+           var currentPiece = [null, null, null, null];
            var startPiece = null;
            var endPiece = null;
            var numberOfPieces = 0;
@@ -24,33 +19,30 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                    board.push([null,null,null,null]);
                } 
                loadImgInBackGround();
-               startingGame();
-               addEventListenerForSquares();
             }
            
+            function start(){
+                startingGame();
+                addEventListenerForSquares();
+            }
            
             function loadImgInBackGround(){
                 for(i = 0; i < 12; i++){
                     switch(i){
-                        case 1: document.getElementById('invisible_piece_a').className = "square moving_a_0";
-                        case 2: document.getElementById('invisible_piece_a1').className = "square moving_a_1";
-                        case 3: document.getElementById('invisible_piece_b').className = "square moving_b_0";
-                        case 4: document.getElementById('invisible_piece_b1').className = "square moving_b_1";
-                        case 5: document.getElementById('invisible_piece_c').className = "square moving_c_0";
-                        case 6: document.getElementById('invisible_piece_c1').className = "square moving_c_1";
-                        case 7: document.getElementById('invisible_piece_d').className = "square moving_d_0";
-                        case 8: document.getElementById('invisible_piece_d1').className = "square moving_d_1";
-                        case 9: document.getElementById('invisible_piece_e').className = "square moving_e_0";
-                        case 10: document.getElementById('invisible_piece_e1').className = "square moving_e_1";
-                        case 11: document.getElementById('invisible_piece_f').className = "square moving_f_0";
-                        case 12: document.getElementById('invisible_piece_f1').className = "square moving_f_1";
+                        case 1: document.getElementById('invisible_piece_a').className = "invisible_piece square moving_a_0";
+                        case 2: document.getElementById('invisible_piece_a1').className = "invisible_piece square moving_a_1";
+                        case 3: document.getElementById('invisible_piece_b').className = "invisible_piece square moving_b_0";
+                        case 4: document.getElementById('invisible_piece_b1').className = "invisible_piece square moving_b_1";
+                        case 5: document.getElementById('invisible_piece_c').className = "invisible_piece square moving_c_0";
+                        case 6: document.getElementById('invisible_piece_c1').className = "invisible_piece square moving_c_1";
+                        case 7: document.getElementById('invisible_piece_d').className = "invisible_piece square moving_d_0";
+                        case 8: document.getElementById('invisible_piece_d1').className = "invisible_piece square moving_d_1";
+                        case 9: document.getElementById('invisible_piece_e').className = "invisible_piece square moving_e_0";
+                        case 10: document.getElementById('invisible_piece_e1').className = "invisible_piece square moving_e_1";
+                        case 11: document.getElementById('invisible_piece_f').className = "invisible_piece square moving_f_0";
+                        case 12: document.getElementById('invisible_piece_f1').className = "invisible_piece square moving_f_1";
                     }
                 }
-/*
-                for(j = 0; j < 12; j++){
-                    document.querySelector('.invisible_piece')[j].style.visibility = 'hidden';
-                }
-*/
             }
 
 
@@ -82,78 +74,10 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
 
 
                function addEventListenerForSquares(){
-           
-
-                   
                     var items = document.getElementsByClassName('square');
                     for (var i = 0; i < items.length; i++) {
                         items[i].addEventListener('click', printDetails);
                     }
-
-
-                     /*
-                   document.querySelectorAll("div.square").forEach(function(item) {
-                    item.addEventListener('click', function() {
-                        console.log(this.innerHTML);
-                    });
-                     });
-                   
-                   
-                   
-                  
-                   .addEventListener('click', function(e) {
-                       console.log("You clicked button " + e.target.innerHTML.toLowerCase());
-                       });
-                   
-                   squares[0].addEventListener('click', () => {
-                       changeSquareByClick(0);
-                   }); 
-                   squares[1].addEventListener('click', () => {
-                       changeSquareByClick(1);
-                   }); 
-                   squares[2].addEventListener('click', () => {
-                       changeSquareByClick(2);
-                   }); 
-                   squares[3].addEventListener('click', () => {
-                       changeSquareByClick(3);
-                   });  
-                   squares[4].addEventListener('click', () => {
-                       changeSquareByClick(4);
-                   }); 
-                   squares[5].addEventListener('click', () => {
-                       changeSquareByClick(5);
-                   }); 
-                   squares[6].addEventListener('click', () => {
-                       changeSquareByClick(6);
-                   }); 
-                   squares[7].addEventListener('click', () => {
-                       changeSquareByClick(7);
-                   }); 
-                   squares[8].addEventListener('click', () => {
-                       changeSquareByClick(8);
-                   }); 
-                   squares[9].addEventListener('click', () => {
-                       changeSquareByClick(9);
-                   }); 
-                   squares[10].addEventListener('click', () => {
-                       changeSquareByClick(10);
-                   }); 
-                   squares[11].addEventListener('click', () => {
-                       changeSquareByClick(11);
-                   }); 
-                   squares[12].addEventListener('click', () => {
-                       changeSquareByClick(12);
-                   }); 
-                   squares[13].addEventListener('click', () => {
-                       changeSquareByClick(13);
-                   }); 
-                   squares[14].addEventListener('click', () => {
-                       changeSquareByClick(14);
-                   }); 
-                   squares[15].addEventListener('click', () => {
-                       changeSquareByClick(15);
-                   });   
-                   */
                }
            
            function startingGame(){
@@ -166,7 +90,6 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                currentPiece = [true, false, true, false];
            
                document.getElementById('current_piece').className = "square p_a";
-               //document.getElementById('current_piece').innerHTML = currentPiece;
            }
            
 
@@ -198,39 +121,36 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
            
            function generateNewPiece(squareNumber){
                let classpicked = "";
-               let randomNumber = Math.random(0,10);
-               if(randomNumber < 0.15) {
-                   currentPiece = [true, false, true, false];
-                   classpicked = "p_a";
+               console.log(lol+"==="+currentPiece.typeOf);
+               if(currentPiece[0] == true && currentPiece[1] == false && currentPiece[2] == true && currentPiece[3] == false) {
+                currentPiece = [false, true, false, true];
+                classpicked = "p_b";
                }
-               else if(randomNumber > 0.15 && randomNumber < 0.35 ) {
-                   currentPiece = [false, true, false, true];
-                   classpicked = "p_b";
+               else if(currentPiece[0] == false && currentPiece[1] == true && currentPiece[2] == false && currentPiece[3] == true) {
+                currentPiece = [false, true, true, false];
+                classpicked = "p_c";
                }
-               else if(randomNumber > 0.35 && randomNumber < 0.5 ) {
-                   currentPiece = [false, true, true, false];
-                   classpicked = "p_c";
+               else if(currentPiece[0] == false && currentPiece[1] == true && currentPiece[2] == true && currentPiece[3] == false) {
+                currentPiece = [false, false, true, true];
+                classpicked = "p_d";
                }
-               else if(randomNumber > 0.5 && randomNumber < 0.65 ) {
-                   currentPiece = [false, false, true, true];
-                   classpicked = "p_d";
+               else if(currentPiece[0] == false && currentPiece[1] == false && currentPiece[2] == true && currentPiece[3] == true) {
+                currentPiece = [true, true, false, false];
+                classpicked = "p_e";
                }
-               else if(randomNumber > 0.65 && randomNumber < 0.8 ) {
-                   currentPiece = [true, true, false, false];
-                   classpicked = "p_e";
+               else if(currentPiece[0] == true && currentPiece[1] == true && currentPiece[2] == false && currentPiece[3] == false) {
+                currentPiece = [true, false, false, true];
+                classpicked = "p_f";
                }
-               else if(randomNumber > 0.8 && randomNumber < 1 ) {
-                   currentPiece = [true, false, false, true];
-                   classpicked = "p_f";
+               else if(currentPiece[0] == true && currentPiece[1] == false && currentPiece[2] == false && currentPiece[3] == true) {
+                currentPiece = [true, false, true, false];
+                classpicked = "p_a";
                }
                document.getElementById('current_piece').className = "square "+classpicked;
-              // document.getElementById('current_piece').innerHTML = currentPiece;
-               console.log(randomNumber);
                lol++;
                if(lol ==13){
                    go();
-               }
-                
+               }               
            }
            
            let lol = 0;
@@ -241,7 +161,7 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
            function fluidIsMoving(){
                interval = setInterval(function(){
                console.log("en cours");
-               let conclusion = checkNextPiecePosition();
+               let conclusion = checkNextPiecePosition();      
                if(!conclusion){
                    clearInterval(interval);
                    console.log("end");
@@ -275,8 +195,10 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                //haut
                if(direction == 0){
                    if(pieceNumber == 0 || pieceNumber == 1 || pieceNumber == 2 || pieceNumber == 3 || pieceNumber == 4 || pieceNumber == 5 || pieceNumber == 6 || pieceNumber == 7){
+                       animateTube(pieceNumber, 0);
                        return false;
                    } else if(board[pieceNumber-8][2] == true) {
+                        animateTube(pieceNumber, 0);
                        if(!checkWinCondition(pieceNumber-8)){
                            console.log("win");
                            return false;
@@ -284,17 +206,19 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                        nextPiece = pieceNumber-8;
                        previousDirection = 2;
                        secondDirection = newDirection(nextPiece);
-                       animateTube(pieceNumber, 0);
                        return true;
                    } else if(board[pieceNumber-8][2] == false) {
+                       animateTube(pieceNumber, 0);
                        return false;
                    }
                }
                //droite
                if(direction == 1){
                    if(pieceNumber == 7 || pieceNumber == 15 || pieceNumber == 23 || pieceNumber == 31 || pieceNumber == 39 || pieceNumber == 47 || pieceNumber == 55 || pieceNumber == 63){
-                       return false;
+                        animateTube(pieceNumber, 1);
+                        return false;
                    } else if(board[pieceNumber+1][3] == true) {
+                    animateTube(pieceNumber, 1);
                        if(!checkWinCondition(pieceNumber+1)){
                            console.log("win");
                            return false;
@@ -302,17 +226,19 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                        nextPiece = pieceNumber+1;
                        previousDirection = 3;
                        secondDirection = newDirection(nextPiece);
-                       animateTube(pieceNumber, 1);
                        return true;
                    } else if(board[pieceNumber+1][3] == false) {
+                    animateTube(pieceNumber, 1);
                        return false;
                    }
                }
                //bas
                if(direction == 2){
                    if(pieceNumber == 56 || pieceNumber == 57 || pieceNumber == 58 || pieceNumber == 59 || pieceNumber == 60 || pieceNumber == 61 || pieceNumber == 62 || pieceNumber == 63){
-                       return false;
+                    animateTube(pieceNumber, 2);
+                    return false;
                    } else if(board[pieceNumber+8][0] == true) {
+                    animateTube(pieceNumber, 2);
                        if(!checkWinCondition(pieceNumber+8)){
                            console.log("win");
                            return false;
@@ -320,17 +246,19 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                        nextPiece = pieceNumber+8;
                        previousDirection = 0;
                        secondDirection = newDirection(nextPiece);
-                       animateTube(pieceNumber, 2);
                        return true;
                    } else if(board[pieceNumber+8][0] == false) {
+                    animateTube(pieceNumber, 2);
                        return false;
                    }
                }
                //gauche
                if(direction == 3){
                    if(pieceNumber == 0 || pieceNumber == 8 || pieceNumber == 16 || pieceNumber == 24 || pieceNumber == 32 || pieceNumber == 40 || pieceNumber == 48 || pieceNumber == 56){
-                       return false;
+                    animateTube(pieceNumber, 3);
+                    return false;
                    } else if(board[pieceNumber-1][1] == true) {
+                    animateTube(pieceNumber, 3);
                        if(!checkWinCondition(pieceNumber-1)){
                            console.log("win");
                            return false;
@@ -338,9 +266,9 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
                        nextPiece = pieceNumber-1;
                        previousDirection = 1;
                        secondDirection = newDirection(nextPiece);
-                       animateTube(pieceNumber, 3);
                        return true;
                    } else if(board[pieceNumber-1][1] == false) {
+                    animateTube(pieceNumber, 3);
                        return false;
                    }
                }
@@ -463,33 +391,3 @@ var board = [[null, null, null, null],[null, null, null, null],[null, null, null
 
            */
 
-
-
-           /* 
-           OLD
-           
-           formule pour trouver la pièce suivante a aller 
-           
-           --- haut ---
-           
-           index - 4 = donne la bonne pièce en haut 
-           sauf
-           pour les index 0, 1, 2, 3 = game over
-           
-           
-           --- droite ---
-           index + 1 = donne la pièce à droite 
-           sauf
-           pour les index 3, 7, 11, 15 = game over
-           
-           --- en bas ---
-           
-           index + 4 = donne la bonne pièce en bas 
-           sauf
-           pour les index 12, 13, 14, 15 = game over
-           
-           --- a gauche ---
-           index - 1 = donne la pièce à gauche 
-           sauf
-           pour les index 0, 5, 9, 13 = game over
-           */
